011ca54e3a1a49d305e730a3e71c20004f8a4fab
Arsen0203 <matosmon0203@gmail.com>
2023-03-24 18:09:22 +0600

    react homework3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// Homework 3
+class Counter extends Component {
+    constructor(props) {
+        super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        this.state = {
+            count: 0
+        };
+    }
+
+    incrementCounter = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    decrementCounter = () => {
+        this.setState({ count: this.state.count - 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.incrementCounter}>+1</button>
+                <button onClick={this.decrementCounter}>-1</button>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Counter;
